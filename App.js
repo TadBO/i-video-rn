db12c2668dd5163730ecf35dff4e878bc574b2c0
unknown <986520756@qq.com>
2019-09-26 11:18:07 +0800

    优化选择框

--- App.js
+++ App.js
@@ -7,35 +7,38 @@
  */
 
 import React, {Fragment, useState, useRef, useEffect} from 'react';
-import {
-    Colors,
-} from 'react-native/Libraries/NewAppScreen';
-import {StyleSheet} from 'react-native';
 import {WebView} from 'react-native-webview';
-import { Picker,  Dialog, Button,View, Text  } from 'react-native-ui-lib';
+import { Picker,  Dialog, Button,View, Text, Toast  } from 'react-native-ui-lib';
 import ActionButton from 'react-native-action-button';
 import {source} from "./utils";
 
 const App = () => {
     const [visible, setVisible] = useState(false);
     const [selectUrI, setSelectUrI] = useState('');
-    const [uri, setUri] = useState('https://m.v.qq.com/');
+    const [channelValue, setChannelValue] = useState({});
+    const [sourceValue, setSourceValue] = useState({});
     const [webUri, setWebUri] = useState('https://m.v.qq.com/');
     const [currentUrl, setCurrentUrl] = useState('');
     const [platformlist, setPlatformlist] = useState([]);
     const [list, setList] = useState([]);
     const [canGoBack, setCanGOBack] = useState(false);
+    const [showToast, setShowToast] = useState(false);
     const webView = new useRef(null);
     useEffect(() => {
         source.getAllList().then(({data}) => {
             const {platformlist, list} = data;
             setPlatformlist(platformlist);
             setList(list);
+            // 设置初始值
+            const channel = platformlist[0];
+            setChannelValue({ label: channel.name, value: channel.url});
+            const sourse = list[0];
+            setSourceValue({label: sourse.name, value: sourse.url});
         });
     }, []);
-    // 返回
+    // 返回源视频
     const handleClick = () => {
-        const newUrl = /\?url=/.test(webUri) ? webUri.split('?url=')[1] : webUri;
+        const newUrl = /\?url=/.test(currentUrl) ? currentUrl.split('?url=')[1] : currentUrl;
         setWebUri(newUrl);
     }
     // 当视频源切换的时候
@@ -44,18 +47,19 @@ const App = () => {
         const { value } = e;
         setSelectUrI('');
         setWebUri(value);
-        setUri(value);
         setVisible(false);
+        setChannelValue(e);
     };
     // 解析源更改时
     const handelSourceChange = (e) => {
         const { value } = e;
         setSelectUrI(value);
+        setSourceValue(e);
     };
     // webview 的地址发生改变的时候，缓存当前的url,用于解析
     const handleOnLoad = (e) => {
         const { url, canGoBack  } = e;
-        setCurrentUrl(url);
+        setCurrentUrl(/\?url=/.test(url) ? url.split('?url=')[1] : url);
         setCanGOBack(canGoBack);
     };
     // 解析时触发
@@ -66,24 +70,24 @@ const App = () => {
         setWebUri(newUri);
         setVisible(false);
     }
-    // 前进
-    const handlegoClick = () => {
-        webView.current.goForward();
-    }
     // 打开控制页
     const handleSetting = () => {
        setVisible(true);
     }
+    // 打开页面失败后处理
+    const handleErrorEvent = () => {
+      setShowToast(true);
+    }
     return (
         <Fragment>
             <View style={{flex: 1}}>
-                <WebView mixedContentMode="compatibility" allowsFullscreenVideo={true} ref={webView} source={{uri: webUri}} onNavigationStateChange={handleOnLoad}/>
+                <WebView mixedContentMode="compatibility" allowsFullscreenVideo={true} ref={webView} source={{uri: webUri}} onNavigationStateChange={handleOnLoad} onError={handleErrorEvent}/>
                 <ActionButton onPress={handleSetting} buttonColor="rgba(231,76,60,1)" position="left"></ActionButton>
             </View>
             <Dialog useSafeArea visible={visible} onDismiss={() => {setVisible(false)}}   height="40%">
                <View useSafeArea style={{ backgroundColor: '#fff', padding: 10}}>
                    <Text>切换不同的视频源，可以找到更多你想看的内容哦~</Text>
-                   <Picker useSafeArea onChange={handleChannelChange}   style={{marginTop: 10}} value={uri} >
+                   <Picker useSafeArea onChange={handleChannelChange}   style={{marginTop: 10}} value={channelValue} >
                        {
                            platformlist.map((item, index) => {
                                return <Picker.Item key={index} label={item.name} value={item.url} />
@@ -91,72 +95,29 @@ const App = () => {
                        }
                    </Picker>
                    <Text>多切换几条解析源，可以提高破解几率哦~</Text>
-                   <Picker useSafeArea onChange={handelSourceChange} value={selectUrI} style={{marginTop: 10}}>
+                   <Picker useSafeArea onChange={handelSourceChange} value={sourceValue} style={{marginTop: 10}}>
                        {
                            list.map((item, index) => {
                                return <Picker.Item key={index} label={item.name} value={item.url}/>
                            })
                        }
                    </Picker>
-                   <Text>选择解析源后破解哦~</Text>
+                   <Text>选择想看的视频和解析源后再破解哦~</Text>
                    <Button disabled={!selectUrI} label="破解" onPress={handleClickTitle} style={{marginTop: 10}}/>
                    {canGoBack && /\?url=/.test(webUri) && <Button label="返回原视频" onPress={handleClick} style={{marginTop: 10}}/>}
                </View>
             </Dialog>
+            <Toast
+                visible={showToast}
+                message='解析失败,请重新选择'
+                position={'top'}
+                centerMessage
+                actions={[
+                    {label: '我知道了', onPress: () => setShowToast(false)},
+                ]}
+            />
         </Fragment>
     );
 };
-
-const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    actionButtonIcon: {
-        fontSize: 20,
-        height: 22,
-        color: 'white',
-    },
-    engine: {
-        position: 'absolute',
-        right: 0,
-    },
-    body: {
-        backgroundColor: Colors.white,
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-    footer: {
-        color: Colors.dark,
-        fontSize: 12,
-        fontWeight: '600',
-        padding: 4,
-        paddingRight: 12,
-        textAlign: 'right',
-    },
-    picker: {
-        flex: 1,
-        padding: 0,
-        height: 32,
-    },
-    button: {
-        margin: 10,
-    }
-});
 
 export default App;
